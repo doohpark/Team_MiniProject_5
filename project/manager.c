9cd78b12840fe7a2cbd5bea98c4d6115f8c942a2
doohpark <82192969+doohpark@users.noreply.github.com>
2021-05-03 10:20:45 +0900

    Update manager.c

--- project/manager.c
+++ project/manager.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "manager.h"
 
-int listClothes(
+int selectMenu(){}
+int listClothes(Clothes *p,int count){}
+int selectDataNo(Clothes *p, int count){}   
+void saveData(Clothes *p, int count){}
+int loadData(Clothes *p){}
+void searchType(Clothes *p, int count){}
+void searchPrice(Clothes *p, int count){}
+void searchStar(Clothes *p, int count){}
+void searchColor(Clothes *p, int count){}
+void searchMaker(Clothes *p, int count){}
