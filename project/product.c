16bc90a52462b866f6a3b9c070f4dd6ab50286ce
doohpark <82192969+doohpark@users.noreply.github.com>
2021-05-03 10:00:03 +0900

    Create product.c

---
+++ project/product.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "product.h"
+
+int addClothes(Clothes *p){}
+void readClothes(Clothes p){}
+int updateClothes(Clothes *p){}
+int deleteClothes(Clothes *p){}
