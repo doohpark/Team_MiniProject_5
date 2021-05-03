c7ad5626af8686b512a964d3a7803b04a936e223
doohpark <82192969+doohpark@users.noreply.github.com>
2021-05-03 09:54:19 +0900

    Create product.h

---
+++ project/product.h
@@ -0,0 +1,15 @@
+typedef struct{
+    char type[20]; //종류
+    char color[20]; // 색깔
+    int price; // 가격
+    int star; // 별점
+    char size; // 사이즈
+    char maker[20]; // 메이커(회사)
+} ;
+
+int addClothes(Product *p); //옷을 추가하는 함수
+void readClothes(Product p); //옷의 목록을 출력하는 함수
+int updateClothes(Product *p); // 옷의 정보를 수정하는 함수
+int deleteClothes(Product *p); // 옷의 정보를 삭제하는 함수
+
+
