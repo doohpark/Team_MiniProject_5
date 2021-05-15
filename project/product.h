583fedb78e519e75c128be992e9938972f8ff0f0
doohpark <82192969+doohpark@users.noreply.github.com>
2021-05-15 22:07:38 +0900

    Update product.h

--- project/product.h
+++ project/product.h
@@ -11,7 +11,7 @@ typedef struct{
 
 int addClothes(Clothes *c); //옷을 추가하는 함수
 void readClothes(Clothes c); //옷의 정보를 출력하는 함수
-int updateClothes(Clothes *c); // 옷을 수정하는 함수
-int deleteClothes(Clothes *c); // 옷을 삭제하는 함수
+int updateClothes(Clothes *c); // 옷을 정보를 수정하는 함수
+int deleteClothes(Clothes *c); // 옷을 정보를 삭제하는 함수
 
 #endif
